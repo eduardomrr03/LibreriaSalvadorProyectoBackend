3b880b6e7f1296e4710afd06e31ba3c8514f1fc0
eduardomrr03 <eduardomrr03@gmail.com>
2023-07-14 03:30:23 -0500

    Creación de Proyecto y configuración previa de dependencias

---
+++ libreriasalvador/src/main/java/com/arquisoftware/libreriasalvador/LibreriasalvadorApplication.java
@@ -0,0 +1,13 @@
+package com.arquisoftware.libreriasalvador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriasalvadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriasalvadorApplication.class, args);
+	}
+
+}
